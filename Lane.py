5d91dbf50ec5097bdbf01a5cfaeacc993162aff2
Mariam Aggour <73789616+AlienCookie907@users.noreply.github.com>
2022-04-20 20:44:35 +0200

    Update Lane.py

--- Lane.py
+++ Lane.py
@@ -154,6 +154,158 @@ class Lane:
 
         return self.warped_frame
 
+        def get_lane_line_indices_sliding_windows(self, plot=False):
+        """
+        Get the indices of the lane line pixels using the
+        sliding windows technique.
+
+        :param: plot Show plot or not
+        :return: Best fit lines for the left and right lines of the current lane
+        """
+        # Sliding window width is +/- margin
+        margin = self.margin
+
+        frame_sliding_window = self.warped_frame.copy()
+
+        # Set the height of the sliding windows
+        window_height = int(self.warped_frame.shape[0] / self.no_of_windows)
+
+        # Find the x and y coordinates of all the nonzero
+        # (i.e. white) pixels in the frame.
+        nonzero = self.warped_frame.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+
+        # Store the pixel indices for the left and right lane lines
+        left_lane_inds = []
+        right_lane_inds = []
+
+        # Current positions for pixel indices for each window,
+        # which we will continue to update
+        leftx_base, rightx_base = self.histogram_peak()
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+
+        # Go through one window at a time
+        no_of_windows = self.no_of_windows
+
+        for window in range(no_of_windows):
+
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = self.warped_frame.shape[0] - (window + 1) * window_height
+            win_y_high = self.warped_frame.shape[0] - window * window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+            cv2.rectangle(frame_sliding_window, (win_xleft_low, win_y_low), (
+                win_xleft_high, win_y_high), (255, 255, 255), 2)
+            cv2.rectangle(frame_sliding_window, (win_xright_low, win_y_low), (
+                win_xright_high, win_y_high), (255, 255, 255), 2)
+
+            # Identify the nonzero pixels in x and y within the window
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                              (nonzerox >= win_xleft_low) & (
+                                      nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                               (nonzerox >= win_xright_low) & (
+                                       nonzerox < win_xright_high)).nonzero()[0]
+
+            # Append these indices to the lists
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+
+            # If you found > minpix pixels, recenter next window on mean position
+            minpix = self.minpix
+            if len(good_left_inds) > minpix:
+                leftx_current = int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > minpix:
+                rightx_current = int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+
+        # Extract the pixel coordinates for the left and right lane lines
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+
+        # Fit a second order polynomial curve to the pixel coordinates for
+        # the left and right lane lines
+        left_fit = None
+        right_fit = None
+
+        global prev_leftx
+        global prev_lefty
+        global prev_rightx
+        global prev_righty
+        global prev_left_fit
+        global prev_right_fit
+
+        # Make sure we have nonzero pixels
+        if len(leftx) == 0 or len(lefty) == 0 or len(rightx) == 0 or len(righty) == 0:
+            leftx = prev_leftx
+            lefty = prev_lefty
+            rightx = prev_rightx
+            righty = prev_righty
+
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+
+        # Add the latest polynomial coefficients
+        prev_left_fit.append(left_fit)
+        prev_right_fit.append(right_fit)
+
+        # Calculate the moving average
+        if len(prev_left_fit) > 10:
+            prev_left_fit.pop(0)
+            prev_right_fit.pop(0)
+            left_fit = sum(prev_left_fit) / len(prev_left_fit)
+            right_fit = sum(prev_right_fit) / len(prev_right_fit)
+
+        self.left_fit = left_fit
+        self.right_fit = right_fit
+
+        prev_leftx = leftx
+        prev_lefty = lefty
+        prev_rightx = rightx
+        prev_righty = righty
+
+        if plot == True:
+            # Create the x and y values to plot on the image
+            ploty = np.linspace(
+                0, frame_sliding_window.shape[0] - 1, frame_sliding_window.shape[0])
+            left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+            right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+            # Generate an image to visualize the result
+            out_img = np.dstack((
+                frame_sliding_window, frame_sliding_window, (
+                    frame_sliding_window))) * 255
+
+            # Add color to the left line pixels and right line pixels
+            out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+            out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [
+                0, 0, 255]
+
+            # Plot the figure with the sliding windows
+            figure, (ax1, ax2, ax3) = plt.subplots(3, 1)  # 3 rows, 1 column
+            figure.set_size_inches(10, 10)
+            figure.tight_layout(pad=3.0)
+            ax1.imshow(cv2.cvtColor(self.orig_frame, cv2.COLOR_BGR2RGB))
+            ax2.imshow(frame_sliding_window, cmap='gray')
+            ax3.imshow(out_img)
+            ax3.plot(left_fitx, ploty, color='yellow')
+            ax3.plot(right_fitx, ploty, color='yellow')
+            ax1.set_title("Original Frame")
+            ax2.set_title("Warped Frame with Sliding Windows")
+            ax3.set_title("Detected Lane Lines with Sliding Windows")
+            plt.show()
+
+        return self.left_fit, self.right_fit
+    
     def calculate_car_position(self, print_to_terminal=False):
 
         car_location = self.orig_frame.shape[1] / 2
